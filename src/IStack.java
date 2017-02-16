919d87f978db2c8ff38c5f0200db9cb77aea6996
alex-prog <coag@kea.dk>
2017-02-16 13:49:00 +0100

    init

---
+++ src/IStack.java
@@ -0,0 +1,14 @@
+/**
+ * Created by coag on 16-02-2017.
+ */
+public interface IStack {
+    Integer size = 0;
+
+    // now this inteface has comments
+    boolean push(int element);
+    int pop();
+    int peak();
+    int size();
+    boolean clear();
+    //Eduards is happy now
+}
---
+++ src/MyIntStack.java
@@ -0,0 +1,8 @@
+import java.util.Stack;
+
+/**
+ * Created by coag on 16-02-2017.
+ */
+public class MyIntStack implements IStack {
+    
+}
